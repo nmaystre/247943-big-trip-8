47a33dddc2f1590472b1a6ad37f1aa0ee2622710
Natalia <n.d.maistrenko@gmail.com>
2019-03-01 18:37:37 +0200

    i dont know why it isnt working

--- src/createEvent.js
+++ src/createEvent.js
@@ -1,9 +1,11 @@
 import eventData from './data';
 
-const createEvent = (data) => (
+const currentData = eventData();
+
+const createEvent = (currentData) =>
   `<article class="trip-point">
   <i class="trip-icon">🏨</i>
-  <h3 class="trip-point__title">${data.type.title}</h3>
+  <h3 class="trip-point__title">${currentData.type.title}</h3>
   <p class="trip-point__schedule">
     <span class="trip-point__timetable">10:00&nbsp;&mdash; 11:00</span>
     <span class="trip-point__duration">1h 30m</span>
@@ -14,6 +16,6 @@ const createEvent = (data) => (
       <button class="trip-point__offer">Add breakfast +&euro;&nbsp;20</button>
     </li>
   </ul>
-</article>`);
+</article>`;
 
 export default createEvent;
